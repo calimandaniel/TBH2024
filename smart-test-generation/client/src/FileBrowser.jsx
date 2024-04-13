af1ea5c0d1ef81b293e6be321f61738394e03e83
Claudiu Mihai Jechel <jechelclaudiumihai@gmail.com>
2024-04-13 18:23:21 +0300

    new format

--- smart-test-generation/client/src/FileBrowser.jsx
+++ smart-test-generation/client/src/FileBrowser.jsx
@@ -1,12 +1,11 @@
 import React from 'react'
 
-
-
 const FileBrowser = ({ data }) => {
     const renderNodes = (node, depth) => {
+        const dashes = '-'.repeat(depth);
         return (
             <div key={node.name}>
-                <p style={{ marginLeft: `${depth * 20}px` }}>{node.name}, depth: {depth}</p>
+                <p>{depth > 0 && '|'}{dashes}{'> '} {node.name}, depth: {depth}</p>
                 {node.children && node.children.map(child => (
                     <React.Fragment key={child.name}>
                         {renderNodes(child, depth + 1)}
@@ -17,7 +16,7 @@ const FileBrowser = ({ data }) => {
     };
 
     return (
-        <div>
+        <div style={{width: '500px', backgroundColor: 'red',  textAlign: 'left'}}>
             {renderNodes(data, 0)}
         </div>
     )
