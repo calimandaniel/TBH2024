2e77a078200904580416ce1a9b36631419a23dac
Claudiu Mihai Jechel <jechelclaudiumihai@gmail.com>
2024-04-13 22:11:07 +0300

    Fix file name position for the display

--- smart-test-generation/client/src/view/files_manipulation/CodeDisplay.jsx
+++ smart-test-generation/client/src/view/files_manipulation/CodeDisplay.jsx
@@ -1,14 +1,22 @@
 import React from 'react';
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
 import { vscDarkPlus } from 'react-syntax-highlighter/dist/esm/styles/prism';
+import Stack from '@mui/material/Stack';
 
 const CodeDisplay = ({ filename, data }) => {
   return (
     <div style={{ minWidth: '400px', maxWidth: '20vw', overflowX: 'auto' }}>
-      <p>{filename}</p>
-      <SyntaxHighlighter language="python" style={vscDarkPlus}>
-        {data}
-      </SyntaxHighlighter>
+      <Stack
+        direction="column"
+        justifyContent="center"
+        alignItems="flex-start"
+        spacing={0}
+      >
+        <p>{filename}</p>
+        <SyntaxHighlighter language="python" style={vscDarkPlus}>
+          {data}
+        </SyntaxHighlighter>
+      </Stack>
     </div>
   );
 };
